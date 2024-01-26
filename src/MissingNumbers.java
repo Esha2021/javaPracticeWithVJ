f7c9b8560e6c1cf870eb741b4f65445060fc5b11
esha2021 <eeswaricse@gmail.com>
2024-01-26 08:39:35 -0600

    updated

--- src/MissingNumbers.java
+++ src/MissingNumbers.java
@@ -1,23 +1,32 @@
 import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MissingNumbers {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Scanner sc = new Scanner(System.in);
-        int[] numArray = {1, 2, 3,5,6,7,8};
-        for(int i=0;i<=numArray.length;i++){
+        int[] numArray = {1, 2, 3,5,6,7,8 ,10,12,13,14,15,17};
 
-            if(numArray[i]!=i+1)
+       List<Integer> misarray= new ArrayList<Integer>();
+        for(int i=0;i<=numArray.length;i++){
+          int currentnumber=numArray[i];
+          int nextnumber=currentnumber+1;
+            if(numArray[i+1]!=nextnumber)
             {
 
                 System.out.println("it is not Seqeuntial");
-                System.out.println("missing number :"+(i+1));
-                break;
+                System.out.println("missing number :"+  (nextnumber));
+                misarray.add(nextnumber);
+                nextnumber++;
+
 
             }
-
-
-            //System.out.println("Missing number"+ i);
         }
+       for( int j=0;j<misarray.size();j++){
+           System.out.println(misarray.get(j));
+       }
+
     }
 }
+;
